416a2b4b6600249cdd51eae1ab9eed47c8a79778
mrmarmor <ommarmor@gmail.com>
2018-12-26 17:33:01 +0200

    Update setAmount.js
    3rd

--- setAmount.js
+++ setAmount.js
@@ -1,6 +1,6 @@
 <script>
-var newWindow = window.open('https://www.easyform.co.il/?formId=6e656994-911d-e811-8453-e4115bd2894e', 'formUntukUpdate', 'width=400,height=350');
+var newWindow = window.open('https://donate.yadlachim.org/heb/', 'formUntukUpdate', 'width=400,height=350');
 newWindow.onload = function(){
-    newWindow.document.getElementById('field10_text').value = '12345';
+    newWindow.document.getElementById('first_name').value = '12345';
 };
 </script>
