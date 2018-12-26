85a1cad4f32916057ea723f8f28ec140e92ca0b2
mrmarmor <ommarmor@gmail.com>
2018-12-26 17:18:35 +0200

    Add files via upload
    2nd

---
+++ setAmount.js
@@ -0,0 +1,6 @@
+<script>
+var newWindow = window.open('https://www.easyform.co.il/?formId=6e656994-911d-e811-8453-e4115bd2894e', 'formUntukUpdate', 'width=400,height=350');
+newWindow.onload = function(){
+    newWindow.document.getElementById('field10_text').value = '12345';
+};
+</script>
